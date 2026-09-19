7247508ea2c2587d237a10077405fc803dc7273b
Caleb-Harper <CalebHarper@my.unt.edu>
2026-09-18 20:25:18 -0500

    Update resource.h

--- resource.h
+++ resource.h
@@ -1,1 +1,28 @@
+#ifndef RESOURCE_H
+#define RESOURCE_H
 
+#include <string>
+#include <iostream>
+
+
+class Rescource {
+private:
+  std::string resourceID;
+  std::string resourceName;
+  std::string resourceType;
+  bool isAvailabe;
+public:
+  Resource();
+  Resource(const std::string& id, const std::string& name,
+            const std::string& type, bool available);
+
+  std::string getID() const;
+  std::string getName() const;
+  std::string getType() const;
+  bool getAvailablility() const;
+
+  void setAvailability(bool status);
+  void display() const;
+};
+
+#endif 
