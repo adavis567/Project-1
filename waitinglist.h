952d107d34dead9ca8c11f12d2aaed863c94c1aa
Caleb-Harper <CalebHarper@my.unt.edu>
2026-09-18 20:45:19 -0500

    Update waitinglist.h

--- waitinglist.h
+++ waitinglist.h
@@ -1,1 +1,32 @@
+#ifndef WAITINGLIST_H
+#define WAITINGLIST_H
 
+#indlude <string>
+#include <iostream>
+
+struct WaitingNode {
+  std::string studentID;
+  std::string studentName;
+  std::string resourceID;
+  WaitingNode* next;
+
+  WaitingNode(const std::string& sid, const std::string& sname,
+              const std::string& rid)
+    : studentID(sid), studentName(sname), resourceID9rid), next(nullptr) {}
+};
+
+class WaitingList {
+private: 
+  WaitingNode* front;
+  WaitingNode* rear;
+public:
+  WaitingList();
+  bool isEmpty() const;
+  void enqueue(const std::string& studentID,
+              const std::string& studentName;
+              const std::string& resourceID);
+  void dequeue();
+  WaitingNode* peek() const;
+  void display() const;
+};
+#endif
