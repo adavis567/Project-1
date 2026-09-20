3de62a617fb15fb09cf4fd3009b35f0c67417b17
EyobTesfay-git <EyobTesfay@my.unt.edu>
2026-09-20 15:32:50 -0500

    Update cancellationHistory.cpp

--- cancellationHistory.cpp
+++ cancellationHistory.cpp
@@ -1,1 +1,21 @@
+#include "cancellationHistory.h"
+#include <iostream>
+
+using namespace std;
+void CancellationHistory::addCancellation(const Reservation& reservation)
+{
+    cancellationStack.push(reservation);
+}
+bool CancellationHistory::undoCancellation(Reservation& reservation)
+{
+    if (cancellationStack.empty())
+    {
+        return false;
+    }
+
+    reservation = cancellationStack.top();
+    cancellationStack.pop();
+
+    return true;
+}
 
